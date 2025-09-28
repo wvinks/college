d79a85b417887a4b2624ae6870a4c5cd68104690
Vika <vkmoiseienko@gmail.com>
2025-09-28 18:55:36 +0000

    Add the code of the third task

---
+++ task_3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+
+int main () {
+    const char* str = "Hello World!";
+    int length = std::strlen(str);
+    std::cout << "line length: " << length << std::endl;
+
+    return 0;
+
+}
+
+
+
+
