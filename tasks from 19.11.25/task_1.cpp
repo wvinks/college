b6a60ca36eceb66808e000f79e0016d1fe8feea6
Vika <vkmoiseienko@gmail.com>
2025-12-02 16:48:47 +0000

    add the code to the first task

---
+++ tasks from 19.11.25/task_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+ std::cout << " /\\_/\\" << std::endl;
+ std::cout << "( o o )" << std::endl;
+ std::cout << "==_Y_==" << std::endl;
+ std::cout << "  `-'" << std::endl;
+
+return 0;
+}
