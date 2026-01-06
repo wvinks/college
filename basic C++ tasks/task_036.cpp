0e752ccff636caa48e0a40730e558e0c69eaa42d
Vika <vkmoiseienko@gmail.com>
2026-01-06 18:45:54 +0000

    added task to "basic C++ tasks"

---
+++ basic C++ tasks/task_036.cpp
@@ -0,0 +1,27 @@
+// Read N numbers and count how many of them are both two-digit and even //
+
+#include <iostream>
+
+int main() {
+    int n;
+    int count = 0;
+
+    std::cout << "enter how many numbers: ";
+    std::cin >> n;
+
+    std::cout << "enter the numbers: ";
+
+    for (int i = 0; i <= n; i++) {
+         int x;
+         std::cin >> x;
+
+         if (x >= 10 && x%2==0) {
+             count++;
+         }
+    }
+
+    std::cout << "amount of numbers that are both two-digit and even: " << count << std::endl;
+
+return 0;
+}
+ 
