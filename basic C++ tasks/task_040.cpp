7f03fb6907a3bef7b9b5b75e5cd6ee815a796471
Vika <vkmoiseienko@gmail.com>
2026-01-07 21:53:30 +0000

    upd task_040 in "basic C++ tasks"

--- basic C++ tasks/task_040.cpp
+++ basic C++ tasks/task_040.cpp
@@ -8,7 +8,7 @@ int main() {
     std::cout << "enter a number: ";
     std::cin >> n;
 
-    for (int i = 0; i <= n; i++) {
+    for (int i = 0; i < n; i++) {
          std::cout << "Hi!" << std::endl;
     }
 
