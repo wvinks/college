f3eceadaee28b413aab3c633b205c9fa7ceb0416
Vika <vkmoiseienko@gmail.com>
2026-01-07 21:49:23 +0000

    added task to "basic C++ tasks"

---
+++ basic C++ tasks/task_040.cpp
@@ -0,0 +1,16 @@
+// Read an integer N and print N lines with the word "Hi!" //
+
+#include <iostream>
+
+int main() {
+    int n;
+
+    std::cout << "enter a number: ";
+    std::cin >> n;
+
+    for (int i = 0; i <= n; i++) {
+         std::cout << "Hi!" << std::endl;
+    }
+
+return 0;
+}
