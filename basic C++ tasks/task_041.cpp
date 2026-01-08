ef9f0fba9f910cc2fbc125b1f4a56c69a3f5ce5f
Vika <vkmoiseienko@gmail.com>
2026-01-08 21:36:07 +0000

    added task to "basic C++ tasks"

---
+++ basic C++ tasks/task_041.cpp
@@ -0,0 +1,26 @@
+// Read N numbers and count how many of them are odd //
+
+#include <iostream>
+
+int main() {
+    int n;
+    int count = 0;
+
+    std::cout << "enter how many numbers: ";
+    std::cin >> n;
+
+    std::cout << "enter a numbers: ";
+
+    for (int i; i <= n; i++) {
+         int x;
+         std::cin >> x;
+
+         if (x%2 == 1) {
+             count++;
+         }
+    }
+
+    std::cout << "amount odd numbers: " << count << std::endl;
+
+return 0;
+}
