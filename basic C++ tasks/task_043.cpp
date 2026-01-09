18f32b5d74342f604c38fa59dc907d9e55a9029f
Vika <vkmoiseienko@gmail.com>
2026-01-09 14:22:00 +0000

    added task to "basic C++ tasks"

---
+++ basic C++ tasks/task_043.cpp
@@ -0,0 +1,28 @@
+// Read a number N and count how many digits in it are equal to the last digit //
+
+#include <iostream>
+
+int main() {
+    int n;
+    int count = -1;
+
+    std::cout << "enter a number: ";
+    std::cin >> n;
+
+    int ld;
+    ld = n%10;
+
+    for (int i = 0; i <= n; i++) {
+         int d;
+         d = n%10;
+
+         if (d == ld) {
+             count++;
+         }
+    n = n/10;
+    }
+
+    std::cout << "amount of digits that equal the last digit: " << count << std::endl;
+
+return 0;
+}
