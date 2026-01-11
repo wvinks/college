083171e323da08b9a0e40308db6d7e0b6e9938ed
Vika <vkmoiseienko@gmail.com>
2026-01-11 16:37:47 +0000

    added task to "basic C++ tasks"

---
+++ basic C++ tasks/task_044.cpp
@@ -0,0 +1,26 @@
+// Read N numbers and count how many of them are negative or zero //
+
+#include <iostream> 
+
+int main() {
+    int n;
+    int count = 0;
+
+    std::cout << "enter how many numbers: ";
+    std::cin >> n;
+
+    std::cout << "enter a numbers: ";
+
+    for (int i = 0; i <= n; i++) {
+         int x;
+         std::cin >> x;
+
+         if (x <= 0) {
+             count++;
+         }
+    }
+
+    std::cout << "amount of numbers that are negative or equal to zero: " << count << std::endl;
+
+return 0;
+}
