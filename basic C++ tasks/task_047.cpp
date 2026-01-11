14551e815e3b25741d67234455a706cd7a9d0a5c
Vika <vkmoiseienko@gmail.com>
2026-01-11 18:45:44 +0000

    added task to "basic C++ tasks"

---
+++ basic C++ tasks/task_047.cpp
@@ -0,0 +1,15 @@
+// Read a number and print its absolute value //
+
+#include <iostream>
+#include <cmath>
+
+int main () {
+    int n;
+
+    std::cout << "enter a number: ";
+    std::cin >> n;
+
+    std::cout << "modulus of a number: " << std::abs(n) << std::endl;
+
+return 0;
+}
