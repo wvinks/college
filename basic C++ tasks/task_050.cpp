317ffc5926eb7c6f3ff827900382b6f786e51a2c
Vika <vkmoiseienko@gmail.com>
2026-01-12 11:01:02 +0000

    added task to "basic C++ tasks"

---
+++ basic C++ tasks/task_050.cpp
@@ -0,0 +1,23 @@
+// Read two numbers and print the smaller one
+
+#include <iostream>
+
+int main() {
+    int a,b;
+
+    std::cout << "enter the first number: ";
+    std::cin >> a;
+
+    std::cout << "enter the second number: ";
+    std::cin >> b;
+
+    if (a > b) {
+        std::cout << "smaller number: " << b << std::endl;
+    }
+
+    else {
+         std::cout << "smaller number: " << a << std::endl;
+    }
+
+return 0;
+}
