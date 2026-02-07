1163cb84caa230f3ad6e75b0df8961eefb32f93a
Vika <vkmoiseienko@gmail.com>
2026-02-07 21:43:44 +0000

    added task to pointers

---
+++ pointers/pointers & dynamic arrays practice/task_018.cpp
@@ -0,0 +1,50 @@
+// Find all indexes of a given number in a dynamic array
+
+#include <iostream>
+
+int main() {
+    int n;
+
+    std::cout << "enter how manu numbers: ";
+    std::cin >> n;
+
+    int* arr = new int[n];
+
+    std::cout << "enter a numbers: ";
+    for (int i = 0; i < n; i++) {
+         std::cin >> arr[i];
+    }
+
+    int num;
+
+    std::cout << "enter the number whoose indices you want to find: ";
+    std::cin >> num;
+
+    int count = 0;
+    int* index_arr = new int[n];
+
+    for (int i = 0; i < n; i++) {
+         if (arr[i] == num) {
+             index_arr[count] = i;
+             count++;
+         }
+    }
+
+    if (count == 0) {
+        std::cout << "number not found" << std::endl;
+    }
+
+    else {
+         std::cout << "indices at which the number you specify is located: ";
+         for (int i = 0; i < count; i++) {
+              std::cout << index_arr[i] << " ";
+         }
+    }
+
+    std::cout << std::endl;
+
+    delete[] arr;
+    delete[] index_arr;
+
+return 0;
+}
