8ecdc400a597070af14f14428afa5e95c71a11b2
wvinks <vkmoiseienko@gmail.com>
2026-03-19 22:14:07 +0200

    added task to "template"

---
+++ template/task_001.cpp
@@ -0,0 +1,17 @@
+// Напишіть шаблонну функцію, що знаходить мінімум двох значень
+
+template<typename T>
+
+        T my_min(T a, T b) {
+                 if (a < b) {
+                     return a;
+                 }
+               
+                 else {
+                      return b;
+                 }
+        }
+
+int main() {
+    return 0;
+}
